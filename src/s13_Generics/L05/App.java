1f88cd5412daf7f435590b160c2fd9a2cbfa0e92
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-02-21 22:28:58 -0300

    format output

--- src/s13_Generics/L05/App.java
+++ src/s13_Generics/L05/App.java
@@ -13,6 +13,7 @@ public class App {
         list.add(5);
         list.add(6);
         Data data = new Data();
+        System.out.println("Print integer list");
         data.printListData(list);
 
         List<String> list1 = new ArrayList<>();
@@ -20,11 +21,14 @@ public class App {
         list1.add("Mariana");
         list1.add("Juliana");
         list1.add("Ana");
+        System.out.println("Print string list");
         data.printListData(list1);
 
         String[] stringArray = {"One","Two","Three"};
+        System.out.println("print String array");
         data.printArrayData(stringArray);
         Integer[] integerArray = {1,2,3,4,5};
+        System.out.println("Print integer array");
         data.printArrayData(integerArray);
 
     }
